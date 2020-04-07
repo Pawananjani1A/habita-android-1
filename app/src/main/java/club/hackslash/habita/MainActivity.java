b23ad8612609ddc099e592b51cf5b7c09bea67d0
Pawananjani1A <pawananjanimth1@gmail.com>
2020-04-07 16:42:28 +0530

    Add plants and disease scanner layouts with navigation enabled.

--- app/src/main/java/club/hackslash/habita/MainActivity.java
+++ app/src/main/java/club/hackslash/habita/MainActivity.java
@@ -2,13 +2,28 @@ package club.hackslash.habita;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+   Button addPlants;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addPlants=(Button)findViewById(R.id.addPlants);
+        addPlants.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri;
+                Intent intent = new Intent(MainActivity.this, AddpPlants.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
